9ea92ffe93a91669c5719676cece7d79f9db0b6e
kaglion <azanguembh@gmail.com>
2022-08-29 16:42:08 +0100

    manage cookies

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const inputs = document.querySelectorAll('input');
 const btns = document.querySelectorAll('button');
 const infoTxt = document.querySelector('.info-txt');
 const affichage = document.querySelector('.affichage');
+let cookiesExist = false;
+
 const today = new Date();
 const nextWeek = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000 );
 let day = ('0' + nextWeek).slice(9,11)
@@ -35,7 +37,35 @@ function btnAction(params) {
 
 
     function creerCookies(name, value, exp) {
-        
+        infoTxt.innerText = ''
+
+        // Check cookie same name 
+        let cookies = document.cookie.split(';')
+        cookies.forEach((cookie) =>{
+            cookie = cookie.trim()
+            let formatCookies = cookie.split('=')
+
+            if (formatCookies[0] === encodeURIComponent(name) ) {
+               cookiesExist = true
+            }
+            console.log(formatCookies);
+
+        })
+
+        if (cookiesExist){
+            infoTxt.innerText = 'Cookies name exit !'
+            cookiesExist = false
+            return
+        }
+
+        // check cookie name
+        if(name.length === 0){
+            infoTxt.style.color = '#550923'
+            infoTxt.innerText = "Impossible to create no name cookie !";
+            
+            return;
+        }
+
         document.cookie = `${encodeURIComponent(name)}=${encodeURIComponent(value)}; expire = ${exp}`
 
         let info = document.createElement('li');
